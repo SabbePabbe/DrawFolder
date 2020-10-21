f5b9d40ca2961ac32689b1dabc4131fc34eb5867
Sabrina Samuelsson <sabrina.sabbe@hotmail.com>
2020-10-21 16:09:11 +0200

    add click listener

--- GestureUnlock/app/src/main/java/com/example/gestureunlock/ui/gallery/GalleryFragment.kt
+++ GestureUnlock/app/src/main/java/com/example/gestureunlock/ui/gallery/GalleryFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.gestureunlock.R
 
 class GalleryFragment : Fragment() {
@@ -23,7 +26,13 @@ class GalleryFragment : Fragment() {
                 ViewModelProviders.of(this).get(GalleryViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_gallery, container, false)
         val imageView: ImageView = root.findViewById(R.id.imageView2)
-        imageView.onClick
+        imageView.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_gallery_to_gestureFragment)
+        }
+
+
+
         return root
     }
+
 }
--- GestureUnlock/app/src/main/java/com/example/gestureunlock/ui/gesture/GestureFragment.kt
+++ GestureUnlock/app/src/main/java/com/example/gestureunlock/ui/gesture/GestureFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -23,6 +25,20 @@ class GestureFragment : Fragment() {
 //        galleryViewModel =
 //            ViewModelProviders.of(this).get(GalleryViewModel::class.java)
         val root = inflater.inflate(R.layout.gesture, container, false)
+        val progressBar: ProgressBar = root.findViewById(R.id.progressBar)
+
+        val btn1 : Button = root.findViewById(R.id.button)
+        btn1.setOnClickListener {
+            progressBar.setProgress(25)
+        }
+
+        val btn2 : Button = root.findViewById(R.id.button2)
+        btn2.setOnClickListener {
+            progressBar.setProgress(100)
+        }
+
+
+
 //        val textView: TextView = root.findViewById(R.id.text_gallery)
 //        gestureViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
